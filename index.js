64416c6d896951fbd18b68e81f24036b6080059d
Aymar Dossou <dossaymar@outlook.com>
2022-10-07 16:47:10 -0500

    added Index.html and index.js files

---
+++ index.js
@@ -0,0 +1,8 @@
+let accountBalance = 51.2;
+let itsColdOutside = true;
+let numberOfFriends = 5;
+let customerFirstName = 'Aymar';
+let streetAddress = '3244 Novel Street';
+
+console.log(accountBalance);
+console.log(accountBalance * numberOfFriends);
